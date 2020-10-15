3aae3434ef30654fb9c1b26e09ebf929d51a6eb0
Panc4kes <jack.sun4@student.education.wa.edu.au>
2020-10-15 18:03:45 +0800

    origin

--- script.js
+++ script.js
@@ -7,8 +7,45 @@ function responsive_nav() {
   }
 }
 
-window.addEventListener('resize', function() {
-  document.getElementById("container").clientHeight =
-  document.getElementById("landing").clientHeight;
-  console.log(document.getElementById("landing").clientHeight)
+function landingMargin() {
+  var landingHeight = (document.getElementById("landing").clientHeight - 220).toString()+"px";
+  document.getElementById("main").style.marginTop = landingHeight;
+}
+
+
+// attach a submit handler to the form
+$("#contact").submit(function(event) {
+
+  // stop form from submitting normally
+  event.preventDefault();
+
+  // get the action attribute from the <form action=""> element
+  var $form = $(this),
+    url = $form.attr('action');
+
+   // cors proxy
+  var corsProxy = "https://jack-task8-cors.herokuapp.com/"
+  // Send the data using post with element id
+  var posting = $.post(corsProxy+url, {
+    fname: $('#fname').val(),
+    lname: $('#lname').val(),
+    email: $('#email').val(),
+    subject: $('#subject').val(),
+    content: $('#msgcontent').val(),
+    submit: $('#submit').val()
+  });
+
+  // Alerts the results
+  posting.done(function(data) {
+    $('#result').text('success');
+  });
+
+  posting.fail(function() {
+    $('#result').text('failed');
+  });
 });
+
+function init() {
+  landingMargin();
+}
+window.addEventListener('resize', landingMargin);
