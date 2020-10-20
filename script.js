23cff2796a55a1509f282a19692f09c573d5c8b2
Panc4kes <jack.sun4@student.education.wa.edu.au>
2020-10-20 23:20:27 +0800

    no summary

--- script.js
+++ script.js
@@ -20,41 +20,70 @@ $("#contact").submit(function(event) {
 
   // get the action attribute from the <form action=""> element
   var $form = $(this),
-    url = $form.attr('action');
+    url = $form.attr("action");
 
    // cors proxy
   var corsProxy = "https://jack-task8-cors.herokuapp.com/"
   // Send the data using post with element id
   var posting = $.post(corsProxy+url, {
-    fname: $('#fname').val(),
-    lname: $('#lname').val(),
-    email: $('#email').val(),
-    subject: $('#subject').val(),
-    content: $('#msgcontent').val(),
-    submit: $('#submit').val()
+    fname: $("#fname").val(),
+    lname: $("#lname").val(),
+    email: $("#email").val(),
+    subject: $("#subject").val(),
+    content: $("#msgcontent").val(),
+    submit: $("#submit").val()
   });
 
   // Alerts the results
   posting.done(function(data) {
-    $('#result').text('success');
+    $("#result").text("success");
   });
 
   posting.fail(function() {
-    $('#result').text('failed');
+    $("#result").text("failed");
   });
 });
 
 
+function append_json(data) {
+  //Set Up the template
+  var s = $("#template")[0].innerHTML.trim();
+  var holder = document.createElement("div");
+  holder.innerHTML = s;
+  var template = holder.childNodes;
+
+  data.forEach(function(object) {
+
+    //Clone Template
+    var newItem = $(template).clone();
+
+    //Populate it
+    $(newItem).find(".name").html("Product Name: " + object.name);
+    $(newItem).find(".company").html("Company: " + object.company);
+    $(newItem).find(".price").html("Price: $" + object.price.toFixed(2));
+    $(newItem).find(".description").html("Description: " + object.description);
+    $(newItem).find(".thumb").attr("src", "assets/images/" + object.imageSrc).attr("alt", object.alt);
+
+    //Append it
+    $(".store").append(newItem);
+  });
+}
+
 $(document).ready(() => {
-  $('#brands .slick').slick({
+  $("#brands .slick").slick({
     autoplay: true,
     autoplaySpeed: 2000,
     speed: 200,
-
-slidesToShow: 4,
-
+    slidesToShow: 4,
     swipeToSlide: true,
   });
+  append_json(products);
 });
 
-window.addEventListener('resize', landingMargin);
+function storeUpdate() {
+  document.getElementById("amount").innerHTML = "$" + cartLS.total().toFixed(2) + " ";
+  document.getElementById("quantity").innerHTML = + cartLS. + "item(s)";
+}
+
+storeUpdate()
+window.addEventListener("resize", landingMargin);
---
+++ store.js
@@ -0,0 +1,12 @@
+products =
+[
+  {
+    "id": 0,
+    "name": "Nintendo 64",
+    "company": "Nintendo",
+    "price": 69,
+    "imageSrc": "placeholder.png",
+    "alt": "N64",
+    "description": "pog console.",
+  },
+];
